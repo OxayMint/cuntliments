97a86b400194da2278b9fdd1f6fb984159d580c6
Farhad Gojazade <farhad.g@bestcomp.net>
2023-01-18 23:43:42 +0400

    added allowed list filtering

--- telegram_bot.js
+++ telegram_bot.js
@@ -7,7 +7,9 @@ const token = '5842425234:AAElCs43QWJ21ufpJ5ZrF2zrox9SPPv8Jjo';//@Cuntliments_bo
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-
+const allowedList = [
+    "fgoja"
+];
 module.exports = {
     init: () => {
         bot.on('callback_query', function onCallbackQuery(callbackQuery) {
@@ -29,6 +31,10 @@ module.exports = {
 
 
         bot.onText(/\/add_cuntliment/, (msg, match) => {
+            if (!allowedList.includes(msg.chat.username)) {
+                bot.sendMessage(msg.chat.id, 'Люблю тебя, малышка, но тебе нельзя так делать');
+                return;
+            }
             var text = msg.text.replace('\/add_cuntliment ', '');
             mongodb.addCuntliment(text).then(res => {
                 bot.sendMessage(msg.chat.id, 'Спасибо, текст \"' + text + '\" успешно добавлен');
@@ -57,7 +63,6 @@ function sendCuntlimentTo(chatId) {
     mongodb.getCuntliment().then(res => {
         console.log("sending cuntliment: " + res.text);
         bot.sendMessage(chatId, res.text, requestCuntlimentButton);
-
         // bot.setChatMenuButton({chat_id: chatId, })
         // bot.inline
     });
